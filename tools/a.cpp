0d8833c8e3fdcbac1a77505c482320f55476f1d4
rykazari <ryota.books@outlook.jp>
2025-02-23 14:44:56 +0900

    updated

--- tools/a.cpp
+++ tools/a.cpp
@@ -1,2 +1,34 @@
 #include <iostream>
 #include <vector>
+
+using namespace std;
+
+vector<string> split(const string &s, char div= ' '){
+	vector<string> res;
+	string parts;
+	for (char ch: s) {
+		if (ch == div){
+			if (parts.size())
+				res.push_back(parts);
+			parts.clear();
+		}
+		else {
+			parts+= ch;
+		}
+	}
+	if (parts.size())
+		res.push_back(parts);
+	return res;
+}
+
+int main(){
+	string s;
+	while(getline(cin,s)){
+		auto inputs = split(s);
+		if(inputs.size()>4)
+			for(int i = 4 ; i < int(inputs.size()); i++)
+				inputs[3]+=" "+inputs[i];
+		printf("|**%s**|%s|%s|%s|\n", inputs[0].c_str(), inputs[1].c_str(), inputs[2].c_str(), inputs[3].c_str());
+	}
+	return 0;
+}
